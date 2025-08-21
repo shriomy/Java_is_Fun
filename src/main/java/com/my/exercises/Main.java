d74cc0909bf688323bc0a75035dc242f365cb5f6
shriomy <snimashafernandopulle@gmail.com>
2025-08-21 17:10:28 +0530

    main class

---
+++ src/main/java/com/my/exercises/Main.java
@@ -0,0 +1,9 @@
+package com.my.exercises;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+// In Java, every application begins with a class name, and that class must match the filename.
--- src/main/java/com/my/exercises/UserOutput.java
+++
@@ -1,7 +0,0 @@
-package com.my.exercises;
-
-public class UserOutput {
-    public static void main(String[] args) {
-        System.out.println("testing ... DONE!");
-    }
-}
