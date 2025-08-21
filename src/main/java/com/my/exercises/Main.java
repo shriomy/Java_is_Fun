416276d7ab50be2c7541e4dae9e3c9e9c4c6b905
shriomy <snimashafernandopulle@gmail.com>
2025-08-21 17:21:19 +0530

    outputting texts and numbers

--- src/main/java/com/my/exercises/Main.java
+++ src/main/java/com/my/exercises/Main.java
@@ -3,7 +3,11 @@ package com.my.exercises;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(5);
+        System.out.println(5 + 10);
     }
 }
 
 // In Java, every application begins with a class name, and that class must match the filename.
+// every program must contain the main() method.//
+//outputting texts should be in double quotes. but not arithmetic numbers
