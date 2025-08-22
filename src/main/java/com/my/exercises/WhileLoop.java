54db4d9358e8461464a313ed5b2a54e8db008331
shriomy <snimashafernandopulle@gmail.com>
2025-08-22 06:37:40 +0530

    While loop

---
+++ src/main/java/com/my/exercises/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.my.exercises;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+    }
+}
