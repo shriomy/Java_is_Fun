4b7375ff60ef9752c85fba845ef79836e440656b
shriomy <snimashafernandopulle@gmail.com>
2025-08-22 15:54:59 +0530

    Modifiers

---
+++ src/main/java/com/my/exercises/Modifiers.java
@@ -0,0 +1,15 @@
+package com.my.exercises;
+
+public class Modifiers {
+
+    public static void main(String[] args) {
+        System.out.println("Non-access modifiers \n" +
+                "final classes - The class cannot be inherited by other classes \n" +
+                "abstract classes - The class cannot be used to create objects (To access an abstract class, it must be inherited from another class \n" +
+                "final attributes and methods - Attributes and methods cannot be overridden/modified\n" +
+                "static attributes and methods - belongs to the class, rather than an object\n" +
+                "abstract - Can only be used in an abstract class, and can only be used on methods. The method does not have a body, for example abstract void run();. The body is provided by the subclass (inherited from).\n" +
+                "synchronized methods - Methods can only be accessed by one thread at a time\n" +
+                "volatile attributes - The value of an attribute is not cached thread-locally, and is always read from the \"main memory\"\n");
+    }
+}
