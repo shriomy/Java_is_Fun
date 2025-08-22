926ed4f806fde0f91e7bf2baf8b0a80ef10868ab
shriomy <snimashafernandopulle@gmail.com>
2025-08-22 06:31:21 +0530

    Math operations

---
+++ src/main/java/com/my/exercises/SimpleMath.java
@@ -0,0 +1,14 @@
+package com.my.exercises;
+
+
+public class SimpleMath {
+    public static void main(String[] args) {
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.random());
+        System.out.println((int)(Math.random() * 101));
+
+    }
+}
